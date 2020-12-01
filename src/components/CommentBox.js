95c67410cddde6b219db70177ea70bdc38e34f36
Jordan <jlewis2008@live.com>
2020-12-01 17:02:48 -0500

    added fetching comments from API

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -18,6 +18,7 @@ class CommentBox extends Component {
   };
   render() {
     return (
+      <div>
       <form onSubmit={this.handleSubmit}>
         <h4>Add a Comment</h4>
         <textarea onChange={this.handleChange} value={this.state.comment}/>
@@ -25,6 +26,8 @@ class CommentBox extends Component {
           <button>Submit Comment</button>
         </div>
       </form>
+      <button className="fetch-comments" onClick={this.props.fetchComments}>Fetch Comments</button>
+      </div>
     );
   }
 }

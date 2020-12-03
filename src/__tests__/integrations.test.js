22e1572bba3c4105a86cf1044dd0e382273a5a0f
Jordan <jlewis2008@live.com>
2020-12-03 11:30:50 -0500

    completed HOC

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import moxios from 'moxios';
 import Root from 'Root';
 import App from 'components/App';
 
-it('can fetch a list of comments and display them', () => {
-  // Attempt to render the *entire* app
+beforeEach(() => {
+  moxios.install();
+  moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+    status: 200,
+    response: [{ name: 'Fetched #1' }, { name: 'Fetched #2' }]
+  });
+});
+
+afterEach(() => {
+  moxios.uninstall();
+})
+
+it('can fetch a list of comments and display them', (done) => {
   const wrapped = mount(
     <Root>
       <App />
     </Root>
   )
-  // fin the 'fetchComments' button and click it
-    wrapped.find('.fetch-comments').simulate('click');
-  // Expect to find a list of comments
-  expect(wrapped.find('li').length).toEqual(500);
+  wrapped.find('.fetch-comments').simulate('click');
+    // introduce a TINY little pause
+  moxios.wait(() => {
+    wrapped.update();
+    expect(wrapped.find('li').length).toEqual(2);
+    done();
+  });
 })
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Root from 'Root';
 import App from 'components/App.js';
 
 ReactDOM.render(
 <Root>
-  <App />
+  <BrowserRouter>
+    <Route path="/" component={App} />
+  </BrowserRouter>
 </Root>, 
 document.getElementById("root"))
